c527afa117db27909e0f34e0e17d8737c6af56e2
npqr <96120993+npqr@users.noreply.github.com>
2024-06-08 09:32:54 +0530

    First run

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
-#include "glad/glad.h"
-#include "GLFW/glfw3.h"
-#include "config.h"
+#include "../includes/glad/glad.h"
+#include "../includes/GLFW/glfw3.h"
+#include "../includes/config.h"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
-int main()
-{
+int main() {
     // Initialise GLFW, and if it doesn't raise error
     if (!glfwInit()) {
         fprintf(stderr, "Failed to initialize GLFW\n");
         return -1;
     }
 
-    glfwWindowHint(GLFW_SAMPLES, 4); // anti aliasing (for multisampling)
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // openGL major version to be 3
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0); // minor set to 3, which makes the version 3.3
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // for MAC OS only
-    glfwWindowHint(GLFW_OPENGL_COMPAT_PROFILE, GLFW_OPENGL_CORE_PROFILE); //avoid using old openGL
+    glfwWindowHint(GLFW_SAMPLES, 4);                                       // anti aliasing (for multisampling)
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);                         // openGL major version to be 3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);                         // minor set to 3, which makes the version 3.3
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);                   // for MAC OS only
+    glfwWindowHint(GLFW_OPENGL_COMPAT_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // avoid using old openGL
 
     GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_NAME, NULL, NULL);
 
@@ -33,14 +32,12 @@ int main()
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        frprintf(stderr, "Failed to Initialize GLAD.\n");
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        fprintf(stderr, "Failed to Initialize GLAD.\n");
         return -1;
     }
 
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window)) {
         processInput(window);
 
         glClearColor(0.85f, 0.74f, 1.0f, 1.0f);
@@ -54,13 +51,11 @@ int main()
     return 0;
 }
 
-void processInput(GLFWwindow* window)
-{
+void processInput(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
-{
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
